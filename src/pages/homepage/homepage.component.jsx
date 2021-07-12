8336232a1e9d6b9512280c78da52321404df6358
AnukaChitisvili <anichitishvili1@gmail.com>
2021-07-13 03:31:38 +0400

    feat: broken into smaller components and styled them

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Directory from '../../directory/directory.component.jsx'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+
+<div className='homepage'>
+<Directory/>
+
+</div>
+
+)
+
+export default HomePage;
